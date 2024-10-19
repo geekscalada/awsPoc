ca32e88baf02dad0611d6fc3498e99612c866692
JoseE <geekscalada@gmail.com>
2024-10-19 14:22:56 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import { DynamoDB } from "aws-sdk";
+
+const dynamoDb = new DynamoDB.DocumentClient();
+
+export const handler = async (event) => {
+  const params = {
+    TableName: "Users",
+    Item: {
+      userId: "123", // Aquí puedes usar un valor dinámico
+      name: "John Doe",
+    },
+  };
+
+  try {
+    await dynamoDb.put(params).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "User added!" }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Could not add user" }),
+    };
+  }
+};
